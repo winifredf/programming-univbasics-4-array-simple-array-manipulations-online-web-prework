7598909ad8119a4d9df901c8f0afaf930f059be3
Winifred Frierson <winifred.dykes@hotmail.com>
2019-09-29 23:36:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,3 +2,8 @@ def colors_in_the_rainbow
   colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
     next_color = "magenta"
     colors_in_the_rainbow.push(next_color)
+
+end
+
+def colors_in_the_rainbow
+  
