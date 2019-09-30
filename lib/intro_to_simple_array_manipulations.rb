7deaa2b5bc4f7124ddfaebef51cbe17fec50ab68
Winifred Frierson <winifred.dykes@hotmail.com>
2019-09-30 00:04:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,25 +1,7 @@
-def 
-  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-    next_color = "magenta"
-    updated_array = using_push(colors_in_the_rainbow, next_color)
-    
-end
-
-def colors_in_the_rainbow
-  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-  colors_in_the_rainbow.pop
+def using_unshift(burrough_in_nyc) = [Brooklyn, Queens, Manhattan, Bronx], new_neighborhood
+  boroughs_in_nyc.unshift(new_neighborhood)
 
 end
 
 
-def colors_in_the_rainbow
-  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-  next_color = "violet"
-  updated_array = using_push
-
-end
-
-
-
-  
   
