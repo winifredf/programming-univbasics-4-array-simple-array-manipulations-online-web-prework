af09b10653b3fa3ef1807418af55989748df753d
Winifred Frierson <winifred.dykes@hotmail.com>
2019-09-30 00:57:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,6 +42,13 @@ end
 
 
 
-def using_concat(my_favorite_things)
+def using_concat(my_favorite_things) = ["raindrops on roses", "whiskers on kittens"]
+    more_favs = ["sports cars", "flatiron school"]
+    all_my_favs = using_concat(my_favorite_things, more_favs)
+
+end
 
 
+
+
+def using_insert
